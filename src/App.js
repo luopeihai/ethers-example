b10a3213d0f083918523bb4c43e70e6504faf8f0
Peihai Luo （罗培海） <phluo@trip.com>
2023-09-24 22:43:46 +0800

    feat: init commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import { ethers } from "ethers";
+import Lock from "./artifacts/contracts/Lock.sol/Lock.json";
 
 function App() {
+  const connect = async () => {
+    let signer = null;
+    let provider;
+
+    if (window.ethereum == null) {
+      console.log("MetaMask not installed; using read-only defaults");
+      provider = ethers.getDefaultProvider();
+    } else {
+      provider = new ethers.BrowserProvider(window.ethereum);
+      signer = await provider.getSigner();
+    }
+    // 当前访问人address
+    const addr = await signer.getAddress();
+    console.log("addr", addr);
+  };
+
+  const readMessage = async () => {
+    const provider = new ethers.BrowserProvider(window.ethereum);
+    const lock = new ethers.Contract(
+      "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512",
+      Lock.abi,
+      provider
+    );
+    const message = await lock.message();
+    alert(message);
+  };
+
+  const setMessage = async () => {
+    const provider = new ethers.BrowserProvider(window.ethereum);
+    const signer = await provider.getSigner();
+    let lock = new ethers.Contract(
+      "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512",
+      Lock.abi,
+      signer
+    );
+    let transaction = await lock
+      .connect(signer)
+      .setMessage("world hello!");
+    let result = await transaction.wait();
+    alert(result.logs[0].args);
+    console.log("result.logs[0].args", result.logs[0].args);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>
+        <button onClick={connect}>connect wallet</button>
+      </p>
+      <p>
+        <button onClick={readMessage}>readMessage</button>
+      </p>
+      <p>
+        <button onClick={setMessage}>setMessage</button>
+      </p>
     </div>
   );
 }
